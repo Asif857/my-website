98187d76f680087db98477851d25a533b82de651
Assiph <asif857@gmail.com>
2024-01-09 14:11:54 +0200

    Changed projects.js so it will be default export.

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,7 @@
-export const PROJECTS = [
+const PROJECTS = [
   ["Tic-Tac-Toe", "https://github.com/Asif857/07-tic-tac-toe-starting-project","In this project you build a Tic-Tac-Toe game using React."],
   ["Practice Project",
   "https://github.com/Asif857/01-starting-project/tree/main", "This was a practice project, creating an investment calculator using React"],
 ];
+
+export default PROJECTS;

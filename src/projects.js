f18fdedb5c23541430d9d033726a11d4846e60fc
Assiph <asif857@gmail.com>
2024-01-15 15:20:32 +0200

    added another project

--- src/projects.js
+++ src/projects.js
@@ -1,7 +1,19 @@
 //[Project Name (title), Link on github, Description]
 
 export const PROJECTS = [
-  ["Tic-Tac-Toe", "https://github.com/Asif857/07-tic-tac-toe-starting-project","In this project you build a Tic-Tac-Toe game using React."],
-  ["Practice Project",
-  "https://github.com/Asif857/01-starting-project/tree/main", "This was a practice project, creating an investment calculator using React"],
+  [
+    "Tic-Tac-Toe",
+    "https://github.com/Asif857/07-tic-tac-toe-starting-project",
+    "In this project you build a Tic-Tac-Toe game using React.",
+  ],
+  [
+    "Practice Project",
+    "https://github.com/Asif857/01-starting-project/tree/main",
+    "This was a practice project, creating an investment calculator using React",
+  ],
+  [
+    "My-Website",
+    "https://github.com/Asif857/my-website]",
+    "This is my website project",
+  ],
 ];

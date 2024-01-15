761157190c6e0212cf917ce6c1e7d0f6cb371849
Assiph <asif857@gmail.com>
2024-01-15 15:19:21 +0200

    added projects

--- src/projects.js
+++ src/projects.js
@@ -1,3 +1,5 @@
+//[Project Name (title), Link on github, Description]
+
 export const PROJECTS = [
   ["Tic-Tac-Toe", "https://github.com/Asif857/07-tic-tac-toe-starting-project","In this project you build a Tic-Tac-Toe game using React."],
   ["Practice Project",
